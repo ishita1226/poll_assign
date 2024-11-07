e2c9eaca70512ef3ca41804c8f41d26b21c46677
Ishita Shukla <ishitashukla120603@gmail.com>
2024-11-07 10:38:29 +0530

    vote done

--- lib/models/poll/poll_bloc.dart
+++ lib/models/poll/poll_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:abhiman_assignment/models/poll/poll.dart';
 import 'package:abhiman_assignment/models/poll/poll_card.dart';
 import 'package:abhiman_assignment/models/poll/poll_event.dart';
 import 'package:abhiman_assignment/models/poll/poll_state.dart';
@@ -14,13 +15,65 @@ class PollBloc extends Bloc<PollEvent, PollState> {
     on<ToggleLike>(_onToggleLike);
   }
 
+  Future<void> _onVotePoll(VotePoll event, Emitter<PollState> emit) async {
+    if (state is PollLoaded) {
+      final currentState = state as PollLoaded;
+      final updatedPolls = currentState.polls.map((poll) {
+        if (poll.id == event.pollId) {
+          final updatedOptions = poll.options.map((option) {
+            if (option.id == event.optionId) {
+              return PollOption(
+                id: option.id,
+                text: option.text,
+                votes: option.votes + 1,
+              );
+            }
+            return option;
+          }).toList();
+
+          return Poll(
+            id: poll.id,
+            question: poll.question,
+            options: updatedOptions,
+            createdAt: poll.createdAt,
+            votedUsers: [...poll.votedUsers, 'currentUserId'],
+            likes: poll.likes,
+          );
+        }
+        return poll;
+      }).toList();
+
+      emit(PollLoaded(updatedPolls));
+      await _hiveService.votePoll(event.pollId, event.optionId);
+    }
+  }
+
   Future<void> _onToggleLike(ToggleLike event, Emitter<PollState> emit) async {
-    try {
+    if (state is PollLoaded) {
+      final currentState = state as PollLoaded;
+      final updatedPolls = currentState.polls.map((poll) {
+        if (poll.id == event.pollId) {
+          final likes = List<String>.from(poll.likes);
+          if (likes.contains('currentUserId')) {
+            likes.remove('currentUserId');
+          } else {
+            likes.add('currentUserId');
+          }
+
+          return Poll(
+            id: poll.id,
+            question: poll.question,
+            options: poll.options,
+            createdAt: poll.createdAt,
+            votedUsers: poll.votedUsers,
+            likes: likes,
+          );
+        }
+        return poll;
+      }).toList();
+
+      emit(PollLoaded(updatedPolls));
       await _hiveService.toggleLike(event.pollId);
-      final polls = await _hiveService.getPolls();
-      emit(PollLoaded(polls));
-    } catch (e) {
-      emit(PollError(e.toString()));
     }
   }
 
@@ -37,16 +90,6 @@ class PollBloc extends Bloc<PollEvent, PollState> {
   Future<void> _onAddPoll(AddPoll event, Emitter<PollState> emit) async {
     try {
       await _hiveService.addPoll(event.poll);
-      final polls = await _hiveService.getPolls();
-      emit(PollLoaded(polls));
-    } catch (e) {
-      emit(PollError(e.toString()));
-    }
-  }
-
-  Future<void> _onVotePoll(VotePoll event, Emitter<PollState> emit) async {
-    try {
-      await _hiveService.votePoll(event.pollId, event.optionId);
       final polls = await _hiveService.getPolls();
       emit(PollLoaded(polls));
     } catch (e) {
--- lib/models/poll/poll_options.dart
+++ lib/models/poll/poll_options.dart
@@ -4,61 +4,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'poll.dart';
 import 'package:abhiman_assignment/models/poll/poll_bloc.dart';
 
-// class PollOptions extends StatelessWidget {
-//   final Poll poll;
-
-//   const PollOptions({Key? key, required this.poll}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: poll.options.map((option) {
-//         final percentage = poll.votedUsers.isNotEmpty
-//             ? (option.votes / poll.votedUsers.length) * 100
-//             : 0.0;
-
-//         return GestureDetector(
-//           onTap: () {
-//             if (!poll.votedUsers.contains('currentUserId')) {
-//               context.read<PollBloc>().add(VotePoll(poll.id, option.id));
-//             }
-//           },
-//           child: Container(
-//             margin: EdgeInsets.symmetric(vertical: 8.0),
-//             decoration: BoxDecoration(
-//               border: Border.all(color: Colors.grey),
-//               borderRadius: BorderRadius.circular(8.0),
-//             ),
-//             child: ClipRRect(
-//               borderRadius: BorderRadius.circular(8.0),
-//               child: Stack(
-//                 children: [
-//                   LinearProgressIndicator(
-//                     value: percentage / 100,
-//                     backgroundColor: Colors.transparent,
-//                     minHeight: 45,
-//                   ),
-//                   Padding(
-//                     padding: EdgeInsets.all(12.0),
-//                     child: Row(
-//                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                       children: [
-//                         Text(option.text),
-//                         Text('${percentage.toStringAsFixed(1)}%'),
-//                       ],
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ),
-//         );
-//       }).toList(),
-//     );
-//   }
-// }
-
-
 class PollOptions extends StatelessWidget {
   final Poll poll;
 
@@ -73,17 +18,17 @@ class PollOptions extends StatelessWidget {
       children: poll.options.map((option) {
         final percentage = totalVotes > 0
             ? (option.votes / totalVotes) * 100
-            : 0.0;
+            : option.votes > 0 ? 100.0 : 0.0;
 
         return InkWell(
-          onTap: hasVoted ? null : () {
+          onTap: () {
             context.read<PollBloc>().add(VotePoll(poll.id, option.id));
           },
           child: Container(
             margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
             padding: EdgeInsets.all(16.0),
             decoration: BoxDecoration(
-              color: hasVoted && poll.votedUsers.contains('currentUserId')
+              color: option.votes > 0 
                   ? Colors.blue.withOpacity(0.2)
                   : Colors.transparent,
               border: Border.all(color: Colors.grey.shade300),
@@ -94,7 +39,7 @@ class PollOptions extends StatelessWidget {
                 Expanded(
                   child: Text(option.text),
                 ),
-                if (hasVoted) Text('${percentage.toStringAsFixed(1)}%'),
+                Text('${percentage.toStringAsFixed(1)}%'),
               ],
             ),
           ),
